d1ba564b0c4c65225e4abdded66879d7f65a03e8
isabellasalvi <36828744+isabellasalvi@users.noreply.github.com>
2020-12-04 01:53:34 -0500

    Update performSimpleBackgroupSubtraction.m
    simple background subtraction

--- performSimpleBackgroupSubtraction.m
+++ performSimpleBackgroupSubtraction.m
@@ -1,2 +1,8 @@
-function sbsImage = performSimpleBackgroupSubtraction(grayscaleImage)
+function sbsImage = performSimpleBackgroupSubtraction(B, grayscaleImage, abs_diff_threshold)
+
+ diff = abs(B - grayscaleImage);
+% sbsImage = im2bw(diff, abs_diff_threshold);
+sbsImage = diff > abs_diff_threshold;
+ 
+
 end
