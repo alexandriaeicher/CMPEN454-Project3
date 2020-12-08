0ae90cd88272ff3bbd1ecdf25632606560422bf5
isabellasalvi <36828744+isabellasalvi@users.noreply.github.com>
2020-12-08 13:45:47 -0500

    Update performAdaptiveBackgroundSubtraction.m
    adaptive bs

--- performAdaptiveBackgroundSubtraction.m
+++ performAdaptiveBackgroundSubtraction.m
@@ -1,2 +1,8 @@
-function absImage = performAdaptiveBackgroundSubtraction(grayscaleImage)
+function absImage = performAdaptiveBackgroundSubtraction(adaptB, abs_diff_threshold, adaptgrayscaleImage)
+
+
+
+    diff = abs(adaptB - adaptgrayscaleImage);
+% sbsImage = im2bw(diff, abs_diff_threshold);
+    absImage = diff > abs_diff_threshold;
 end
